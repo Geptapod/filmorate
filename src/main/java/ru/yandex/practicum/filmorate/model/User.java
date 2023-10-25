e557280638e5789fb7375eadf63f53240092b074
Heptapod <vitalik_prime@bk.ru>
2023-10-25 22:49:40 +0300

    feature/add-controllers-and-classes

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class User {
+
+    private Long id;
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    private String email;
+    @NotBlank
+    @Pattern(regexp = "^[^\\s]+$")
+    private String login;
+    private String name;
+    @PastOrPresent
+    private LocalDate birthday;
+}
